185c97769d041c6f418131907e7a6fb0ea8a93c3
JustinMitchell <mitchelljm04@gmail.com>
2022-10-13 13:27:29 -0400

    Assignment Complete

---
+++ lib/user.rb
@@ -0,0 +1,20 @@
+
+
+class User
+
+    attr_accessor :first_name, :last_name
+    
+    # def initialize(name, last_name)
+    #     @name = name
+    #     @last_name = last_name
+    # end
+
+    def first
+        @first_name = first_name
+    end
+
+    def last
+        @last_name = last_name
+    end
+
+end
